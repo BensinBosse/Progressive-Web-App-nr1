5f688340ebb7b0bc3e636a2f9ff1bed6c8106ba8
pontusolsson03 <58231866+pontusolsson03@users.noreply.github.com>
2022-10-17 10:38:39 +0200

    Tappat det helt
    Vet inte hur jag ska lösa skiten

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 console.log("index.js laddat")
 
+let playerInputEl = document.getElementsByClassName("player-input")
+let placeholderEl = playerInput.placeholder
+let inputIdArr = []
+
+for (i = 0; i < playerInputEl.length; i++) {
+    //Vet inte vad jag håller på med alls längre jag har
+    //TAPPAT DET HELT
+    //mvh
+}
+
