0fdaab8d6d60f5bc8923e6fc1f961f26a5f9c0bb
BensinBosse <kallehaljerod@gmail.com>
2022-10-17 10:02:38 +0200

    fixade grunden för map

--- js/ingame.js
+++ js/ingame.js
@@ -1,26 +1,26 @@
 const maps = ["School", "Office", "McDonalds", "Grums", "din mor"];
 let lastmap = null
-let randomMap = parseInt(Math.random() *5);
+let randomMap = parseInt(Math.random() *maps.length);
 let map = maps[randomMap];
-console.log(map);
-
-let i = 0
-
-while (i<10, i++)
-{
 
 
-if (map == lastmap){
-    randomMap = parseInt(Math.random() *5);
+
+for (let i=0; i<10; ++i)
+{    
+    randomMap = parseInt(Math.random() * maps.length)
     map = maps[randomMap]
-    console.log("last map was the same")
+    console.log("map",map)
+    console.log("lastmap",lastmap)
+    console.log("count",i)
 
+    if (map != lastmap){
+        document.getElementById("mapText").innerHTML = map;
+        lastmap = map
+        console.log("hoppas att ja inte ser detta")
+        
+    }
+    else{
+        console.log("else")
+    }
 
-}
-else{
-    lastmap = map 
-    console.log("lastmap is",lastmap);
-}
-console.log(lastmap,"lastmap")
-console.log(map,"map")
 }
