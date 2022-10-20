b7414ea808bc27ef34f2b14dca948df7ea6ce472
KalleDunderBerg <kallehaljerod@gmail.com>
2022-10-21 00:01:24 +0200

    div i div i div i div :)

--- js/ingame.js
+++ js/ingame.js
@@ -3,9 +3,27 @@ let playerArr = players.split(",");
 playerAmount = playerArr.length;
 let i2 = 0;
 
+let skola = [
+  "Skola",
+  "Lärare",
+  "Elev",
+  "Städare",
+  "Mattant",
+  "Rektor",
+  "jfieofjoeijfosidjfoijo",
+];
+let kontor = [
+  "Arbetare",
+  "kenneths mamma",
+  "något mer",
+  "en till sak",
+  "dig sjelv",
+];
+
+let maps = [skola, kontor];
+
 while (i2 < playerAmount) {
   i2++;
-  console.log("hej");
   var playerContainer = document.getElementById("container");
   var playerCard = document.createElement("div");
 
@@ -15,97 +33,112 @@ while (i2 < playerAmount) {
 
   playerContainer.appendChild(playerCard);
 }
+giveRandomOccupation();
+
+function giveRandomOccupation() {
+  i2 = 0;
+  let plats = maps[parseInt(Math.random() * 2)];
+  let lastPlayer = document.getElementsByClassName("item");
+  let player = document.getElementById("");
+  while (i2 < lastPlayer.length) {
+    player = document.getElementById("player" + (i2 + 1));
+    var playerInfo = document.createElement("div");
+
+    playerInfo.setAttribute("id", "player");
+
+    player.appendChild(playerInfo);
+
+    console.log(player);
+    playerCard = Math.random() * plats.length;
+    i2++;
+  }
+}
 
 if (playerAmount < 5) {
-    playerContainer.style.width = "max-content"
+  playerContainer.style.width = "max-content";
 }
 
 //timer settings
-const wholeTimer = document.getElementById("timer")
-const timerSeconds = document.getElementById("timer-seconds")
-const timerMinutes = document.getElementById("timer-minutes")
-const colon = document.getElementById("colon")
+const wholeTimer = document.getElementById("timer");
+const timerSeconds = document.getElementById("timer-seconds");
+const timerMinutes = document.getElementById("timer-minutes");
+const colon = document.getElementById("colon");
 let onVar = false;
 let finishedVar = false;
 let seconds = 0;
 let countdown;
 
-timerSeconds.textContent = seconds
-colon.textContent = ":0"
+timerSeconds.textContent = seconds;
+colon.textContent = ":0";
 
 // !IMPORTANT KALLE!
 //Detta (minutes) ska vara den globala variabeln som sätt i timer.html,
 //bara för nu har jag satt den som 2 för test
-let minutes = 2
-timerMinutes.textContent = minutes
+let minutes = 2;
+timerMinutes.textContent = minutes;
 
 function startTimer() {
-    countdown = setInterval(function(){
-        if(onVar === false){
-            return
-        }
-        else {
-            seconds--;
-            timerSeconds.textContent = seconds;
-            if (seconds <10 && seconds > 1) {
-                colon.textContent = ":0"
-            }
-            else if(seconds == 0 && minutes == 0) {
-                clearInterval(countdown);
-                colon.innerHTML = "TIME´S UP!"
-                timerSeconds.textContent = ""
-                timerMinutes.textContent = ""
-                finishedVar = true
-                colon.setAttribute("#hide")
-            }
-            else if (seconds == -1 ) {
-                minutes--
-                timerMinutes.textContent = minutes
-                seconds = 59
-                timerSeconds.textContent = seconds
-                colon.textContent = ":"
-            }
-        }  
-    }, 1000);
+  countdown = setInterval(function () {
+    if (onVar === false) {
+      return;
+    } else {
+      seconds--;
+      timerSeconds.textContent = seconds;
+      if (seconds < 10 && seconds > 1) {
+        colon.textContent = ":0";
+      } else if (seconds == 0 && minutes == 0) {
+        clearInterval(countdown);
+        colon.innerHTML = "TIME´S UP!";
+        timerSeconds.textContent = "";
+        timerMinutes.textContent = "";
+        finishedVar = true;
+        colon.setAttribute("#hide");
+      } else if (seconds == -1) {
+        minutes--;
+        timerMinutes.textContent = minutes;
+        seconds = 59;
+        timerSeconds.textContent = seconds;
+        colon.textContent = ":";
+      }
+    }
+  }, 1000);
 }
 
 function stopTimer() {
-    clearInterval(countdown);
+  clearInterval(countdown);
 }
 
 //function
-wholeTimer.addEventListener("click", function(){
-    console.log("Timer klickad")
-    if (onVar === false && finishedVar === false) {
-        startTimer()
-        onVar = true
-    }
-    else if (onVar === true && finishedVar === false) {
-        stopTimer()
-        onVar = false
-    }
-})
-
+wholeTimer.addEventListener("click", function () {
+  console.log("Timer klickad");
+  if (onVar === false && finishedVar === false) {
+    startTimer();
+    onVar = true;
+  } else if (onVar === true && finishedVar === false) {
+    stopTimer();
+    onVar = false;
+  }
+});
 
 //function for putting clicked player card in focus
-let inFocus = false
-let focusCard = ""
-playerContainer.onclick = function(event) {
-    let target = event.target;
-    console.log(target)
-    if (inFocus === false && focusCard === "" && target!== playerContainer){
-        target.classList.add("focus-class");
-        inFocus = true
-        focusCard = target
-        console.log("Fokus är " + focusCard)
-    }
-    else if (inFocus === true && focusCard === target){
-        target.classList.remove("focus-class");
-        inFocus = false
-        focusCard = ""
-    }
+let inFocus = false;
+let focusCard = "";
+playerContainer.onclick = function (event) {
+  let target = event.target;
+  console.log(target);
+  if (inFocus === false && focusCard === "" && target !== playerContainer) {
+    target.classList.add("focus-class");
+    inFocus = true;
+    focusCard = target;
+    console.log("Fokus är " + focusCard);
+  } else if (inFocus === true && focusCard === target) {
+    target.classList.remove("focus-class");
+    inFocus = false;
+    focusCard = "";
+  }
 };
 
 if (inFocus === true) {
-    document.querySelector("body").style.background = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)"
+  document.querySelector("body").style.background =
+    "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)";
 }
