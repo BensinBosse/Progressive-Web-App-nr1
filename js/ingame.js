055fb30bf3123208e2d73e651bff6207693607e8
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-10-20 15:57:30 +0200

    Massor av sjaB
    Jajamen

--- js/ingame.js
+++ js/ingame.js
@@ -6,35 +6,63 @@ console.error(typeof(players))
 let playerArr = players.split(",");
 console.log(playerArr)
 
-
-
-
-// import Timer from "easytimer.js";
-
-// const timer = new Timer();
-// /*
 // const maps = ["School", "Office", "McDonalds", "Grums", "din mor"];
 // let lastmap = null
 // let randomMap = parseInt(Math.random() *maps.length);
 // let map = maps[randomMap];
 // */
-// let timerEl = document.getElementById("timer")
-// timerEl.addEventListener("click", function timerClick(){
-//     console.log("Timer klickad")
-//     //Här ska alltså timern startas
-// })
 
-// //Timer logic
-// Timer.start({countdown: true, startValues: {seconds: 3}});
 
-// $('#timer').html(Timer.getTimeValues().toString());
+//timer settings
+let wholeTimer = document.getElementById("timer")
+let timerSeconds = document.getElementById("timer-seconds")
+let timerMinutes = document.getElementById("timer-minutes")
+let colon = document.getElementById("colon")
+let onVar = false;
+finishedVar = false;
+var seconds = timerSeconds.textContent;
+var countdown;
 
-// Timer.addEventListener('secondsUpdated', function (e) {
-//     $('#timer').html(Timer.getTimeValues().toString());
-// });
+function startTimer() {
+    countdown = setInterval(function(){
+        if(onVar === false){
+            return
+        }
+        else {
+            seconds--;
+            timerSeconds.textContent = seconds;
+            if (seconds <10 && seconds > 0) {
+                colon.textContent = ":0"
+            }
+            else if (seconds > 9){
+                colon.textContent = ":"
+            }
+            else if(seconds == 0 && timerMinutes.textContent == "0") {
+                clearInterval(countdown);
+                colon.textContent = "MORSAN"
+                timerSeconds.textContent = ""
+                timerMinutes.textContent = ""
+                finishedVar = true
+            }
+            
+        }  
+    }, 1000);
+}
 
-// Timer.addEventListener('targetAchieved', function (e) {
-//     $('#timer').html('KABOOM!!');
-//     console.log("KLAR")
-// });
+function stopTimer() {
+    clearInterval(countdown);
+}
 
+
+//function
+wholeTimer.addEventListener("click", function(){
+    console.log("Timer klickad")
+    if (onVar === false && finishedVar === false) {
+        startTimer()
+        onVar = true
+    }
+    else if (onVar === true && finishedVar === false) {
+        stopTimer()
+        onVar = false
+    }
+})
