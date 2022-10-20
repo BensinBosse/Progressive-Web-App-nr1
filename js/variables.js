555b364eed16be5d3061b7fd0c324c6a05e0ca6e
KalleDunderBerg <kallehaljerod@gmail.com>
2022-10-21 00:14:12 +0200

    time save grejer

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,14 @@
 var players = [];
 let i = 0;
 
+function saveTime() {
+  localStorage.setItem("savedTime", timerValue);
+}
+
+function getSavedTime() {
+  localStorage.getItem("savedTime");
+}
+
 function bengt() {
   while (i < playerCount) {
     playerCountString = document.getElementById("input-" + (i + 1)).value;
