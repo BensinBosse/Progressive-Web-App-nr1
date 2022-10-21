3accbebd3cb0ee44eaf935ea4b7d62b0c64e247b
BensinBosse <kallehaljerod@gmail.com>
2022-10-21 12:30:37 +0200

    en till ändring som kommer att laga allt :)

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,7 +5,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then((cache) => {
-            return cache.addAll(["./", "manifest.json", "Index.html", "ingame.html", "timer.html", "css/end.css", "css/general.css", "css/index.css", "css/ingame.css","css/timer.css",  "js/index.js", "js/timer.js","js/variables.js", "/src/images/icons/icon-72x72.png", "/src/images/icons/icon-144x144.png", "/src/images/icons/icon-192x192.png"]);
+            return cache.addAll(["./", "manifest.json", "Index.html", "ingame.html", "timer.html", "css/end.css", "css/general.css", "css/index.css", "css/ingame.css","css/timer.css",  "js/index.js", "js/timer.js","js/variables.js", "src/images/icons/icon-72x72.png", "src/images/icons/icon-144x144.png", "src/images/icons/icon-192x192.png"]);
         })
     );
     console.log("install dcode");
