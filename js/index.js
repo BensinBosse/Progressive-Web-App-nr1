d9008d309fe2268a62f4c6474584f4c32683a16b
pontusolsson03 <58231866+pontusolsson03@users.noreply.github.com>
2022-10-22 16:53:52 +0200

    Förbättrad scroll index.html
    Jag har lagt till en funktion som kollar om playerContainer.scrollHeight är större än playerContainer.clientHeight. Om det är sant sätts justify-content på den till "", och om det är falskt sätts den till space-around, vilket gör att player-boxarna ser bättre ut på större skärmar när de är få.

--- js/index.js
+++ js/index.js
@@ -4,19 +4,6 @@ let playerCount = document.getElementsByClassName("player-input").length;
 var playerCountString = "input-" + playerCount;
 console.log(playerCountString);
 
-//navigation
-let nextBtn = document.getElementById("next-btn");
-nextBtn.addEventListener("click", function nextPage() {
-  bengt();
-  console.log(players);
-
-  // För GitHub:
-  // window.location.href = "/progressive-web-app-nr1/timer.html";
-
-  // För dev-server:
-  window.location.href = "/timer.html";
-});
-
 let addBtn = document.getElementById("more-btn");
 let removeBtn = document.getElementById("fewer-btn");
 
@@ -32,9 +19,13 @@ removeBtn.addEventListener("click", function removePlayer() {
   playerCount = playerCount - 1;
 
   playerBox.removeChild(playerBox.lastElementChild);
+  scrollBottom(playerContainer);
+
+  scrollCheck()
 });
 
 let playerContainer = document.getElementById("player-box");
+playerContainer.style.justifyContent = 'space-around';
 function scrollBottom(element) {
   element.scroll({ top: element.scrollHeight, behavior: "smooth" });
 }
@@ -55,6 +46,33 @@ addBtn.addEventListener("click", function addPlayer() {
   playerCount = document.getElementsByClassName("player-input").length;
   console.log(playerCount);
   scrollBottom(playerContainer);
+
+  scrollCheck()
 });
 
 scrollBottom(playerContainer);
+
+//navigation
+let nextBtn = document.getElementById("next-btn");
+nextBtn.addEventListener("click", function nextPage() {
+  bengt();
+  console.log(players);
+
+  // För GitHub:
+  // window.location.href = "/progressive-web-app-nr1/timer.html";
+
+  // För dev-server:
+  window.location.href = "/timer.html";
+});
+
+//Sätter justify-content: space-around på playerContainer,
+//bara sålänge den inte overflowar
+function scrollCheck() {
+  if (playerContainer.scrollHeight > playerContainer.clientHeight) {
+    playerContainer.style.justifyContent = '';
+    console.log("scroll")
+  } else {
+    playerContainer.style.justifyContent = 'space-around';
+    console.log("space")
+  }
+}
